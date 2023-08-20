e5d75186a87314aa27d0f69b39d2106033c635e9
daniilscheifes <daniilscheifes@gmail.com>
2023-08-20 18:58:33 +0200

    change index

--- components/header.js
+++ components/header.js
@@ -4,4 +4,36 @@ class Header extends HTMLElement {
   }
 }
 
+connectedCallback() {
+  this.innerHTML = `
+  <!-- Header -->
+  <header id="header" class="alt">
+    <h1 id="logo"><a href="index.html">Daniil Scheifes </a></h1>
+    <nav id="nav">
+      <ul>
+        <li class="current"><a href="index.html">Home</a></li>
+        <li class="submenu">
+          <a href="#">Academic</a>
+          <ul>
+            <li><a href="phd.html">PhD</a></li>
+            <li><a href="Thesis.html">Thesis topics</a></li>
+            <li><a href="non-academic.html">Non-academic</a></li>
+          </ul>
+        </li>
+                      <li class="submenu">
+          <a href="#">Personal</a>
+          <ul>
+            <li><a href="Movies.html">Movies</a></li>
+            <li><a href="right-sidebar.html">Blog</a></li>
+            <li><a href="no-sidebar.html">More?</a></li>
+          </ul>
+        </li>
+        <li><a href="contact.html" >Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+
+`;
+
 customElements.define('header-component', Header);
