b2df9255adff3e07d1046d1ef2eff7618b79db96
daniilscheifes <daniilscheifes@gmail.com>
2023-08-20 20:27:11 +0200

    header.js

---
+++ header.js
@@ -0,0 +1,30 @@
+const header =  `
+<header id="header" class="alt">
+  <h1 id="logo"><a href="index.html">Daniil Scheifes </a></h1>
+  <nav id="nav">
+    <ul>
+      <li class="current"><a href="index.html">Home</a></li>
+      <li class="submenu">
+        <a href="#">Ancademic</a>
+        <ul>
+          <li><a href="phd.html">PhD</a></li>
+          <li><a href="Thesis.html">Thesis topics</a></li>
+          <li><a href="Teaching.html">Teaching</a></li>
+        </ul>
+      </li>
+                    <li class="submenu">
+        <a href="#">Personal</a>
+        <ul>
+          <li><a href="Movies.html">Movies</a></li>
+          <li><a href="right-sidebar.html">Blog</a></li>
+        </ul>
+      </li>
+      <li><a href="contact.html" >Contact</a></li>
+    </ul>
+  </nav>
+</header>
+`
+document.addEventListener('DOMContentLoaded', (event) => {
+  const homepage = document.querySelector('.homepage');
+  homepage.insertAdjacentHTML('afterbegin', header);
+});
